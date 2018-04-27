00ae5636fc1f525224171046d6bac02ecd36abf3
Leo <oleoneto@gmail.com>
2018-04-27 15:04:53 -0400

    Updated database configuration file.

--- Templated/pythonfiles/dbconfig.py
+++ Templated/pythonfiles/dbconfig.py
@@ -3,7 +3,41 @@
 import sqlite3
 
 db = sqlite3.connect('db.sqlite3')
-connection = db.cursor()
 
-def create_table():
-    connection('CREATE TABLE IF NOT EXIST Users(name TEXT, age REAL)')
+def createTables():
+    Users = """
+        CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY,
+        name VARCHAR(50),
+        username REAL,
+        hash TEXT
+        )"""
+
+    Songs = """
+        CREATE TABLE IF NOT EXISTS Songs(
+        id INTEGER PRIMARY KEY,
+        title VARCHAR(50),
+        artist VARCHAR(50),
+        album VARCHAR(50),
+        artwork VARCHAR(100),
+        preview VARCHAR(100)
+        )"""
+
+    connection = db.cursor()
+    db.execute(Users)
+    db.execute(Songs)
+    # connection.close()
+
+def createSong(title=None, album=None, artist=None, artwork=None, preview=None):
+    if title:
+        connection = db.cursor()
+        connection.execute("INSERT INTO Songs VALUES(null, ?, ?, ?, ?, ?)", (title, album, artist, artwork, preview))
+        db.commit()
+
+def searchSong(keyword=None):
+    connection = db.cursor()
+    if keyword is not None:
+        query = connection.execute("SELECT * FROM Songs WHERE title LIKE ? OR artist LIKE ? OR album LIKE ?", ("%" + keyword + "%","%" + keyword + "%","%" + keyword + "%",))
+    else:
+        query = connection.execute("SELECT * FROM Songs")
+    return query
